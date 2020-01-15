408dbfb00d581d5135dd1820b04075258d38c108
hairrrrr <781728963@qq.com>
2020-01-15 20:22:24 +0800

    6

---
+++ 03_variable/init_many_at_once.go
@@ -0,0 +1,10 @@
+//具体可以参考上一讲，declare_many_at_once
+//see more details,infer refer to declare_many_at_once
+package main
+import "fmt"
+func main(){
+  var a, b, c = 1 ,2 ,3
+  fmt.Println(a,b,c)
+}
+//output
+//1 2 3

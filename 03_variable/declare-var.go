b1bfc815e8a24417af8a7048c37c67157ed45c1e
hairrrrr <781728963@qq.com>
2020-01-15 19:54:42 +0800

    4

---
+++ 03_variable/declare-var.go
@@ -0,0 +1,23 @@
+package main 
+import "fmt"
+func main(){
+  var a int 
+  var b float64
+  var c string
+  var d string 
+  var e bool
+  
+  a = 1025
+  b = 10.25
+  c = "CC"
+  d = "C"
+  e = true
+
+  fmt.Printf("%v \n%v \n%v \n%v \n%v \n", a, b, c, d, e)
+}
+//output
+//1025 
+//10.25 
+//CC 
+//C 
+//true

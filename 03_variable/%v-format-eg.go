1e576f6566c73aff2216ad5e8dacca925b56d103
hairrrrr <781728963@qq.com>
2020-01-15 17:47:34 +0800

    1

--- 03_variable/%v-format-eg.go
+++ 03_variable/%v-format-eg.go
@@ -1,4 +1,4 @@
-//用%v打印表变量的实例
+//用%v打印变量的实例
 //use format of %v to print variables- example
 package main 
 import "fmt"
@@ -13,11 +13,11 @@ func main(){
   i := `M`
   f := `Do you like my hat?`
   //可以看出
-  //1.用%v 打印字符串有两种声明格式：""和``
-  //2.想打出单个字符 声明时可以使用：``或""，使用''不会打出单个字母，打出的是字母对应的utf-8的数字值
+  //1.用%v 打印字符串有两种变量声明格式：""和``
+  //2.想打出单个字符 声明变量时可以使用：``或""，使用''不会打出单个字母，打出的是字母对应的utf-8的数字值
   //we can see:
-  //1.if you want to print string,when you initialize the variables you can use "" or ``
-  //2.if you want to print a single character,you can use `` or "",but do not use ''.in  situatuon of using '',you will get the value of utf-8
+  //1.if you want to print a string,when you initialize the variables you can use "" or ``
+  //2.if you want to print a single character,you can use `` or "",but do not use ''.in the situatuon of using '',you will get the the character's corresponding value of utf-8
   fmt.Printf("%v \n",a)
   fmt.Printf("%v \n",b)
   fmt.Printf("%v \n",c)

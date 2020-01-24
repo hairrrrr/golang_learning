96e780fe83f845982f9291a9e7e9b37b2eceeea0
hairrrrr <781728963@qq.com>
2020-01-24 15:23:52 +0800

    1

---
+++ 05_blank-identifier/01_invalid-code/main.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+  a := "sleep"
+  b := "get up"
+  fmt.Println("a - ",a)
+
+}
+//error:
+//b is not being used - invalid code
---
+++ 05_blank-identifier/01_invalid-code/readme.md
@@ -0,0 +1,3 @@
+所有局部变量都必须被程序用到。不允许多余的局部变量
+
+all local variables must be used
